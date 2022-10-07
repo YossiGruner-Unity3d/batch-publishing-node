6da8428838ba080489cebfcceccb6eead6a61532
Yossi <yossi.gruner@unity3d.com>
2022-10-07 12:13:18 -0600

    Update to lib

---
+++ example/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const publisherPortalLib = require('../lib');
+require('dotenv').config()
+
+async function main() {
+
+    var unity_version = "2020.3.6f1";
+    var file = "./data/openapi.unitypackage";
+    var packageVersion_id = 250770;
+    
+    publisherPortalLib.setConfig(process.env.HOST)
+
+    var keys = await publisherPortalLib.login(process.env.USERNAME, process.env.PASSWORD);
+    console.log(keys);
+    //await lib.getPackageVersion(keys, packageVersion_id)
+    //await lib.deleteDraftPackageVersion(keys, packageVersion_id)
+
+    //await lib.getPublisherLimits(keys)
+    //await lib.getCategoryList(keys)
+    //await lib.uploadUnityPackage(keys, file, packageVersion_id, unity_version)
+}
+
+main()
+    .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
